f0cfa40a2e7e57f925fd098b61804e220b33403d
prasadtara <79865246+prasadtara@users.noreply.github.com>
2026-03-16 14:11:59 -0600

    Update database configuration to use environment variable

--- dev4_cust6_RENDER/crinkle/crinkle/settings.py
+++ dev4_cust6_RENDER/crinkle/crinkle/settings.py
@@ -70,8 +70,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'crinkle.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
     )
 }
 
